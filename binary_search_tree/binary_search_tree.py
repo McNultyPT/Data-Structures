47a8b58aa9daaacbdce90147030b179161bf81f9
McNultyPT <mcnultypt@gmail.com>
2019-05-14 15:50:43 -0700

    completing bst insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,20 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    current = self
+    new_node = BinarySearchTree(value)
+    while current.value != value:
+      if value < current.value:
+        if current.left is not None:
+          current = current.left
+        else:
+          current.left = new_node
+      elif value > current.value:
+        if current.right is not None:
+          current = current.right
+        else:
+          current.right = new_node
+
 
   def contains(self, target):
     pass
