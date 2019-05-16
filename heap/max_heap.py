6511703b465695914f5f0e8f140bcfe7ba3deb00
McNultyPT <mcnultypt@gmail.com>
2019-05-16 15:41:19 -0700

    completing mh get_size and _sift_down with 1 error

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -14,14 +14,26 @@ class Heap:
 
   def get_size(self):
     pass
+    # return len(self.storage)
 
   def _bubble_up(self, index):
     if index == 0:
       return
-    parent_index = ((index + 1) // 2) -1
-    if self.storage[index] > self.storage[parent_index]:
-      self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
-    return self._bubble_up(parent_index)
+    parent = (index - 1) // 2
+    if self.storage[index] > self.storage[parent]:
+      self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+    return self._bubble_up(parent)
 
   def _sift_down(self, index):
-    pass
+    left = index * 2 + 1
+    right = index * 2 + 2
+    length = len(self.storage) - 1
+    if left <= length and self.storage[left] > self.storage[index]:
+      largest = left
+    else:
+      largest = index
+    if right <= length and self.storage[right] > self.storage[largest]:
+      largest = right
+    if largest != index:
+      self.storage[largest], self.storage[index] = self.storage[index], self.storage[largest]
+      self._sift_down(largest)
